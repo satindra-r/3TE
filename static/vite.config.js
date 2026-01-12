5a4667e5c1ff8bf12b552ed7df4e6fcd5e45b9d6
Satindra Ramalingam <satindra.r@gmail.com>
2026-01-12 21:14:27 +0530

    fix sound system

--- static/vite.config.js
+++ static/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
 	base: '/3TE/',
+	publicDir: 'audio',
 	server: {
 		fs: {
 			allow: ['..']
